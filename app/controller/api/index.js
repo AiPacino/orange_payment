978d7a91a4bc1ad6f8fbe89282061fd8ea77a768
lucong <332553882@qq.com>
2018-01-08 19:04:33 +0800

    controller api

--- app/controller/api/index.js
+++ app/controller/api/index.js
@@ -6,7 +6,10 @@ router.get('/' , async (req , res) => {
   res.success({version : '0.0.1'})
 })
 
-
 router.use('/mini' , require('./mini'))
+router.use('/user' , require('./user'))
+router.use('/question' , require('./question'))
+router.use('/answer' , require('./answer'))
+router.use('/payment' , require('./payment'))
 
 module.exports = router
---
+++ app/controller/api/user.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const router = express.Router()
+const {rpcProxy , rpcArgs} = require('./../proxy/rpcClientProxy')
+
+
+module.exports = router
